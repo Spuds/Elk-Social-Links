3a57eeb75220ee5ef5a9d77b882febed85374e48
Spuds <spuds@spudsdesign.com>
2017-11-23 12:25:05 -0600

    ! update FB to new api ! add missing /li ! add support for 1.1 markup

--- sociallinks.subs.php
+++ sociallinks.subs.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2014 Spuds
  * @license Mozilla Public License version 1.1 http://www.mozilla.org/MPL/1.1/.
  *
- * @version 0.2
+ * @version 0.3
  *
  */
 
@@ -76,24 +76,40 @@ function ipdc_sociallinks(&$output, &$message)
 		return;
 	}
 
+	$version = substr(FORUM_VERSION, 8, 3);
+
 	// If this is this the first message in the thread
-	if ($output['id'] == $context['topic_first_message'])
+	if ((int) $output['id'] === (int) $context['topic_first_message'])
 	{
 		loadLanguage('sociallinks');
 
 		// Set the div class
-		$style = empty($output['attachment']) ? 'class="floatleft slink"' : 'class="slink_attach"';
+		if ($version === '1.1')
+		{
+			$style = empty($output['attachment']) ? 'class="slink"' : 'class="slink_attach"';
 
-		$output['body'] .= '
+			$output['body'] .= '
+			</section>
+			<section ' . $style . '>
+				<ul class="sl-buttons">';
+		}
+		else
+		{
+			$style = empty($output['attachment']) ? 'class="floatleft slink"' : 'class="slink_attach"';
+
+			$output['body'] .= '
 			</div>
 			<div ' . $style . '>
 				<ul class="sl-buttons">';
+		}
 
 		// Show Twitter Tweet button
 		if (!empty($modSettings['sl_twitter']))
 		{
 			$output['body'] .= '
-					<li class="sl-t"><a href="https://twitter.com/share" class="twitter-share-button" data-url="' . $scripturl . '?topic=' . $context['current_topic'] . '" data-counturl="' . $scripturl . '?topic=' . $context['current_topic'] . '" data-text="' . $context['page_title_html_safe'] . '"></a></li>';
+					<li class="sl-t">
+						<a href="https://twitter.com/share" class="twitter-share-button" data-url="' . $scripturl . '?topic=' . $context['current_topic'] . '" data-counturl="' . $scripturl . '?topic=' . $context['current_topic'] . '" data-text="' . $context['page_title_html_safe'] . '"></a>
+					</li>';
 
 			loadJavascriptFile('https://platform.twitter.com/widgets.js', array('async' => 'true', 'defer' => 'true'));
 		}
@@ -102,7 +118,9 @@ function ipdc_sociallinks(&$output, &$message)
 		if (!empty($modSettings['sl_googleplus']))
 		{
 			$output['body'] .= '
-					<li class="sl-g"><div class="g-plus" data-action="share" data-annotation="none" data-height"28" data-href="' . $scripturl . '?topic=' . $context['current_topic'] . '"></div></li>';
+					<li class="sl-g">
+						<div class="g-plus" data-action="share" data-annotation="none" data-height"28" data-href="' . $scripturl . '?topic=' . $context['current_topic'] . '"></div>
+					</li>';
 
 			loadJavascriptFile('https://apis.google.com/js/platform.js', array('async' => 'true', 'defer' => 'true'));
 		}
@@ -111,7 +129,9 @@ function ipdc_sociallinks(&$output, &$message)
 		if (!empty($modSettings['sl_linkedin']))
 		{
 			$output['body'] .= '
-					<li class="sl-l"><script type="IN/Share" data-url="' . $scripturl . '?topic=' . $context['current_topic'] . '"></script></li>';
+					<li class="sl-l">
+						<script type="IN/Share" data-url="' . $scripturl . '?topic=' . $context['current_topic'] . '"></script>
+					</li>';
 
 			loadJavascriptFile("https://platform.linkedin.com/in.js", array('async' => 'true', 'defer' => 'true'));
 		}
@@ -120,7 +140,9 @@ function ipdc_sociallinks(&$output, &$message)
 		if (!empty($modSettings['sl_whatsapp']))
 		{
 			$output['body'] .= '
-					<li class="sl-w"><a href="whatsapp://send" data-text="' . $context['page_title_html_safe'] . '" data-href="' . $scripturl . '?topic=' . $context['current_topic'] . '" class="wa_btn wa_btn_s" style="display:none">' . $txt['sl_share'] . '</a>';
+					<li class="sl-w">
+						<a href="whatsapp://send" data-text="' . $context['page_title_html_safe'] . '" data-href="' . $scripturl . '?topic=' . $context['current_topic'] . '" class="wa_btn wa_btn_s" style="display:none">' . $txt['sl_share'] . '</a>
+					</li>';
 
 			loadJavascriptFile("https://cdn.jsdelivr.net/whatsapp-sharing/1.3.3/whatsapp-button.js", array('defer' => 'true'));
 			addInlineJavascript('$(document).ready(function () {WASHAREBTN.crBtn();})', true);
@@ -130,14 +152,27 @@ function ipdc_sociallinks(&$output, &$message)
 		if (!empty($modSettings['sl_telegram']))
 		{
 			$output['body'] .= '
-					<li class="sl-tg"><a href="tg://share?url=' . $scripturl . '?topic=' . $context['current_topic'] . '?t=12&text=' . $context['page_title_html_safe'] . '"><i class="sl-icon icon-tg"></i>' . $txt['sl_share'] . '</a></li>';
+					<li class="sl-tg">
+						<a href="tg://share?url=' . $scripturl . '?topic=' . $context['current_topic'] . '?t=12&text=' . $context['page_title_html_safe'] . '"><i class="sl-icon icon-tg"></i>' . $txt['sl_share'] . '</a>
+					</li>';
 		}
 
 		// Show Facebook Like button
 		if (!empty($modSettings['sl_facebook']))
 		{
 			$output['body'] .= '
-					<li class="sl-f"><iframe src="https://www.facebook.com/plugins/like.php?href=' . $scripturl . '?topic=' . $context['current_topic'] . '&width=50&layout=button&action=like&show_faces=false&share=false&height=20&appId" width="50" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></li>';
+					<li class="sl-f">
+						<div id="fb-root"></div>
+						<div class="fb-like" data-href="' . $scripturl . '?topic=' . $context['current_topic'] . '" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+					</li>';
+
+			addInlineJavascript('(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = \'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11\';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, \'script\', \'facebook-jssdk\'));', true);
 		}
 
 		$output['body'] .= '
